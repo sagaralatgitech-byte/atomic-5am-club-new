dd606fcb3e236dd8f8af0523cd0422ff7e33c4f9
sagar.alatgitech-byte <sagar.alatgitech@gmail.com>
2026-01-10 16:13:45 +0530

    Fix build errors

--- vite.config.js
+++ vite.config.js
@@ -34,6 +34,7 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
+        maximumFileSizeToCacheInBytes: 10 * 1024 * 1024, // 10 MiB to accommodate large icons
         runtimeCaching: [
           {
             urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
